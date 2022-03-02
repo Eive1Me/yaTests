1a00b524f3cc59d7df9178d5e2538feae1102c7a
Eive1Me <83867541+Eive1Me@users.noreply.github.com>
2022-03-02 23:10:14 +0300

    pls

--- src/test/java/sites/selenidetests/PerformancePage.java
+++
@@ -1,9 +0,0 @@
-package sites.selenidetests;
-
-import com.codeborne.selenide.WebDriverRunner;
-
-public class PerformancePage {
-    public boolean rightPage(){
-        return WebDriverRunner.url().equals("https://shelly.kpfu.ru/e-ksu/SITE_STUDENT_SH_PR_AC.student_personal_marks?p_menu=8");
-    }
-}
